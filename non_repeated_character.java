80d22d242b6ff2b15bd79ce0879b2a710353e017
ASHVITHAb <ashvithameena@gmail.com>
2026-06-13 21:18:56 +0530

    Implement method to find first non-repeated character

---
+++ non_repeated_character.java
@@ -0,0 +1,24 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class FirstNonRepeatedChar {
+    public static void main(String[] args) {
+
+        String str = "stress";
+
+        LinkedHashMap<Character, Integer> map = new LinkedHashMap<>();
+
+        for(char ch : str.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        for(Map.Entry<Character, Integer> entry : map.entrySet()) {
+
+            if(entry.getValue() == 1) {
+                System.out.println("First Non-Repeated Character: "
+                                   + entry.getKey());
+                break;
+            }
+        }
+    }
+}
