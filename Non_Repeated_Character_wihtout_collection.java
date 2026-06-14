c2aec93295a5a4e6fb2b6a706cba58827a575cf1
ASHVITHAb <ashvithameena@gmail.com>
2026-06-14 23:37:53 +0530

    Implement method to find first non-repeated character

---
+++ Non_Repeated_Character_wihtout_collection.java
@@ -0,0 +1,16 @@
+public class FirstNonRepeatedChar {
+    public static void main(String[] args) {
+
+        String str = "stress";
+
+        for(int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+
+            if(str.indexOf(ch) == str.lastIndexOf(ch)) {
+                System.out.println(ch);
+                break;
+            }
+        }
+    }
+}
